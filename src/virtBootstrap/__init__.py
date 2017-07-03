65ef323df230b770c56217367e9c019cffcb0a7d
Radostin Stoyanov <rstoyanov1@gmail.com>
2017-07-03 08:27:24 +0100

    Expose bootstrap method from virtBootstrap module
    This allows other python applications to import the virtBootstrap
    module and call the bootstrap() method.
    
    Example:
    >>> import virtBootstrap
    >>> virtBootstrap.bootstrap()

---
+++ src/virtBootstrap/__init__.py
@@ -0,0 +1,26 @@
+"""
+virt-bootstrap - Is a tool providing an easy way to setup the root file system
+                 for libvirt-based containers.
+
+ Authors:
+    Radostin Stoyanov <rstoyanov1@gmail.com>
+
+ Copyright (c) 2017 Radostin Stoyanov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from virtBootstrap.virt_bootstrap import bootstrap
+
+__version__ = "0.1.0"
